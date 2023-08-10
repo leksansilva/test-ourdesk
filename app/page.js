930b587062b564a2ce2786ca7253c71671bb789a
leksansilva <lek.2000@outlook.com>
2023-08-10 13:31:52 -0300

    :recycle: correction in height

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ export default function Home() {
     <iframe
       src="https://sales-frontend.bigdelivery-staging.avantsoft.com.br/"
       width="100%"
-      height="100"
+      height="100%"
     ></iframe>
   );
 }
