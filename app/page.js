cc13611171192e1f72ceaa7757fd77d71e3f565c
leksansilva <lek.2000@outlook.com>
2023-08-10 13:48:51 -0300

    :sparkles: implement payment

--- app/page.js
+++ app/page.js
@@ -1,9 +1,19 @@
+import styles from "./page.module.css";
+
 export default function Home() {
   return (
-    <iframe
-      src="https://sales-frontend.bigdelivery-staging.avantsoft.com.br/"
-      width="100%"
-      height="100%"
-    ></iframe>
+    <div className={styles.main}>
+      <div>
+        <script async src="https://js.stripe.com/v3/pricing-table.js"></script>
+        <stripe-pricing-table
+          pricing-table-id="prctbl_1NdbrME8exeQ2sflDN0lFFKx"
+          publishable-key="pk_test_51NdbjiE8exeQ2sfl8WRpx5BIKZbi6dXN8xMqMnYEbuwFMS5xt3EipVZsZOhpRYJtca0Wm3rKYpXb4IZuDG9bz65B00bzNBWYqK"
+        ></stripe-pricing-table>
+      </div>
+      <iframe
+        className={styles.main}
+        src="https://sales-frontend.bigdelivery-staging.avantsoft.com.br/"
+      ></iframe>
+    </div>
   );
 }
